3d6928f32d937ceb03b958664034ffdf8406cfb1
aguzovatii <guzovatii.anatolii@gmail.com>
2025-02-19 10:28:43 +0200

    Add a page with all pending tasks

--- components/todo-sidebar.tsx
+++ components/todo-sidebar.tsx
@@ -3,11 +3,16 @@
 import * as React from "react";
 import { DatePicker } from "@/components/date-picker";
 import { AppSidebar } from "./app-sidebar";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
-export function TodoSidebar({ date }: { date: Date }) {
+export function TodoSidebar({ date }: { date?: Date }) {
   return (
     <AppSidebar projectIndex={0}>
       <DatePicker date={date} />
+      <Button variant="ghost" className="mx-4">
+        <Link href="/dashboard/todos/all">See all pending tasks</Link>
+      </Button>
     </AppSidebar>
   );
 }
