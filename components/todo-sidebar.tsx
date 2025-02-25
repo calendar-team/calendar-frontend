83c8d48d96566c1b95fcf8219ee7a5113a07d261
aguzovatii <guzovatii.anatolii@gmail.com>
2025-02-25 21:11:09 +0200

    Handle all tasks button when sidebar is collapsed

--- components/todo-sidebar.tsx
+++ components/todo-sidebar.tsx
@@ -4,15 +4,41 @@ import * as React from "react";
 import { DatePicker } from "@/components/date-picker";
 import { AppSidebar } from "./app-sidebar";
 import Link from "next/link";
-import { Button } from "./ui/button";
+import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { LayoutList } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export function TodoSidebar({ date }: { date?: Date }) {
+  const pathname = usePathname();
+
   return (
     <AppSidebar projectIndex={0}>
       <DatePicker date={date} />
-      <Button variant="ghost" className="mx-4">
-        <Link href="/dashboard/todos/all">See all pending tasks</Link>
-      </Button>
+      <SidebarMenu>
+        <SidebarMenuItem className="flex items-center justify-center">
+          <SidebarMenuButton
+            tooltip="All pending tasks"
+            className="hidden group-data-[collapsible=icon]:block"
+            isActive={pathname === "/dashboard/todos/all"}
+          >
+            <Link href="/dashboard/todos/all">
+              <LayoutList size={16} />
+            </Link>
+          </SidebarMenuButton>
+          <SidebarMenuButton
+            tooltip="All pending tasks"
+            className="mx-3 group-data-[collapsible=icon]:hidden"
+            isActive={pathname === "/dashboard/todos/all"}
+          >
+            <Link
+              href="/dashboard/todos/all"
+              className="w-full flex flex-row gap-2 items-center justify-center"
+            >
+              <LayoutList size={16} /> All pending tasks
+            </Link>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
     </AppSidebar>
   );
 }
