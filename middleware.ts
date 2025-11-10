68938a03d55fa83d1d694ef2f40a2e1a702c1f3f
aguzovatii <guzovatii.anatolii@gmail.com>
2025-11-10 20:40:28 +0200

    Upgrade to NEXT.js 16.0.1

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { withAuth } from "next-auth/middleware";
-
-export default withAuth({
-  pages: {
-    signIn: "/welcome",
-  },
-});
-
-export const config = { matcher: ["/((?!welcome|auth/signin|auth/signup).*)"] };
